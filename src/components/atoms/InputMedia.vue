6a2eab4f076f507f581b2ebc4ba6d5c8afb716b1
KimHeeCheol <msn9975@gmail.com>
2018-07-24 23:53:17 +0900

    툴팁버튼 수정중

---
+++ src/components/atoms/InputMedia.vue
@@ -0,0 +1,44 @@
+<template>
+  <span class="button-container">
+    <div>
+      <button>
+        {{ label }}
+      </button>
+    </div>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'InputMedia',
+  props: {
+    label: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .button-container {
+    display: inline-block;
+    position: relative;
+  }
+  button {
+    width: 35px;
+    height: 35px;
+    background-color: transparent;
+    border: 0;
+    outline: none;
+    color: #1DA1F2;
+    cursor: pointer;
+    border-radius: 50%;
+  }
+  button:hover {
+    background-color: rgba(190, 190, 190, 0.1);
+  }
+
+</style>
+
+
+
--- src/components/molecules/TweetingArea.vue
+++ src/components/molecules/TweetingArea.vue
@@ -3,23 +3,40 @@
     <div class="tweet-container">
       <editable-div :class="editableDivStyle"/>
     </div>
+    <div class="tweet-toolbar-container">
+      <div class="tweet-toolbar--left">
+        <input-media v-for="button in mediaButtonList" :key="index" :label="button.label"/>
+      </div>
+      <div class="tweet-toolbar--right">
+
+      </div>
+    </div>
+
   </form>
 </template>
 
 <script>
 import EditableDiv from './../atoms/EditableDiv';
 import TweetTextArea from './../atoms/TweetTextArea';
+import InputMedia from './../atoms/InputMedia';
 
 export default {
   name: 'TweetingArea',
   components: {
     TweetTextArea,
     EditableDiv,
+    InputMedia,
   },
   data: function() {
     return {
       placeholder: '무슨 일이 일어나고 있나요?',
       editableDivStyle: 'tweet-textarea',
+      mediaButtonList: [
+        { label: 'IMG' },
+        { label: 'GIF' },
+        { label: 'XXX' },
+        { label: 'XXX' },
+      ],
     };
   },
 };
@@ -41,5 +58,24 @@ export default {
     border: 1px solid #C6E7FB;
     border-radius: 8px;
     height: 100%;
+  }
+
+  .tweet-toolbar-container {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 37px;
+    border: 1px solid #d7d7d7;
+  }
+  .tweet-toolbar--left {
+    width: 208px;
+    height: 100%;
+    border: 1px solid #d7d7d7;
+  }
+  .tweet-toolbar--right {
+    margin-left: auto;
+    width: 128px;
+    height: 100%;
+    border: 1px solid #d7d7d7;
   }
 </style>
