45fbb7c36f0c201eb03212a963ee11d3d19d5891
heecheolman <msn9975@gmail.com>
2018-08-18 20:56:16 +0900

    팔로우기능 완료

--- src/components/atoms/NicknameList.vue
+++ src/components/atoms/NicknameList.vue
@@ -1,9 +1,17 @@
 <template>
-  <li class="nickname-wrap" @click="linkToProfile">
-    {{ user.nickname }}
+  <li class="list-wrap" @click="linkToProfile">
+    <div class="nickname-wrap">
+      <span class="nickname-text">{{ user.nickname }}</span>
+    </div>
+    <div class="follow-button-wrap">
+      <button class="follow-button" @click="follow" ref="followButton">팔로우</button>
+    </div>
   </li>
 </template>
 <script>
+import axios from 'axios';
+import store from './../../lib/Storage';
+
 export default {
   name: 'NicknameList',
   props: {
@@ -11,31 +19,124 @@ export default {
       type: Object,
     }
   },
+  data() {
+    return {
+      ableClick: true,
+      listId: '',
+    }
+  },
+  mounted() {
+    if(store.user.follow !== null) {
+      for(let i = 0; i < store.user.follow.length; i++) {
+        if(store.user.follow[i] === this.user.id) {
+          this.$refs.followButton.classList.add('disabled');
+          this.ableClick = false;
+        }
+      }
+    } else {
+      store.user.follow = [];
+    }
+  },
   methods: {
     linkToProfile() {
       // 프로필화면 으로
+      // console.log(store.user);
+    },
+    async follow() {
+      if(this.ableClick) {
+        await axios.get(`/api/id/${this.user.nickname}`, {
+          params: {
+            nickname: this.user.nickname,
+          },
+        })
+          .then((result) => {
+            this.listId = result.data[0].id;
+            return this.addFollow();
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      }
+    },
+    async addFollow() {
+      const followPromise = await axios.put(`/api/follow/${store.user.id}/with/${this.listId}`, {
+        params: {
+          user_id: store.user.id,
+          id: this.listId,
+        },
+      })
+        .then(() => {
+          this.ableClick = false;
+          this.$refs.followButton.classList.add('disabled');
+          store.user.follow.push(this.listId);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      return followPromise;
     },
   },
 };
 </script>
 <style scoped>
-  .nickname-wrap {
+  .list-wrap {
     display: block;
     box-sizing: border-box;
     width: 100%;
     height: 40px;
     text-align: center;
-    font-size: 0.8rem;
-    color: #14171a;;
-    font-weight: 400;
     line-height: 40px;
     border-bottom: 1px solid #e2e2e2;
     cursor: pointer;
     border-radius: 4px;
   }
-  .nickname-wrap:hover {
+  .list-wrap:hover {
     background-color: #E8F5FD;
+  }
+
+  .nickname-text {
+    font-size: 0.8rem;
+    color: #14171a;;
+    font-weight: 400;
+  }
+
+  .list-wrap:hover .nickname-text {
     color: #1DA1F2;
     text-decoration: underline;
+  }
+
+  .nickname-wrap {
+    display: inline-block;
+    width: 200px;
+    height: 100%;
+  }
+
+  .follow-button-wrap {
+    display: inline-block;
+    width: auto;
+    height: 100%;
+  }
+  .follow-button {
+    outline: none;
+    border: none;
+    border-radius: 10px;
+    width: 60px;
+    height: 28px;
+    text-align: center;
+    line-height: 28px;
+    background-color: #1DA1F2;
+    color: #fff;
+    font-weight: 500;
+    cursor: pointer;
+    transition: 0.2s;
+  }
+  .follow-button:hover {
+    background-color: #1577b3;
+  }
+
+  .disabled {
+    cursor: not-allowed !important;
+    background-color: #a9a9a9 !important;
+    color: white !important;
   }
 </style>
